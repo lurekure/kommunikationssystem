b9d7880d7c9903db3e6b4a1d75f26840d53ef1ed
lurekure <143624047+lurekure@users.noreply.github.com>
2025-05-15 14:42:12 +0200

    Tog bort skeleton och lade till mallen igen så att jag (Ludvig) kan starta om från början

--- Skeleton/Skeleton.ino
+++ Skeleton/Skeleton.ino
@@ -17,19 +17,13 @@
 // predefined functions
 void l1_send(unsigned long l2frame, int framelen);
 boolean l1_receive(int timeout);
-
 // your own
-void send_byte(byte data);
-bool detect_byte(byte data, int timeout);
-void detect_frame(long data);
-long read_next_bits(long data, int length);
 
 //
 // Runtime
 //
 
 // Runtime variables
-byte LED_PAYLOAD;
 
 // State
 int state = NONE;
@@ -41,7 +35,6 @@ int state = NONE;
 Shield sh;  // note! no () since constructor takes no arguments
 Transmit tx;
 Receive rx;
-long recFrame;
 
 //////////////////////////////////////////////////////////
 
@@ -49,7 +42,6 @@ long recFrame;
 // Code
 //
 void setup() {
-  Serial.begin(9600);
   sh.begin();
 
   //////////////////////////////////////////////////////////
@@ -57,7 +49,7 @@ void setup() {
   // Add init code here
   //
 
-  state = APP_PRODUCE;
+  state = NONE;
   
   // Set your development node's address here
 
@@ -77,8 +69,6 @@ void loop() {
     case L1_SEND:
       Serial.println("[State] L1_SEND");
       // +++ add code here and to the predefined function void l1_send(unsigned long l2frame, int framelen) below
-      l1_send(tx.frame, LEN_FRAME);
-      state = L1_RECEIVE;
 	  
       // ---
       break;
@@ -86,29 +76,14 @@ void loop() {
     case L1_RECEIVE:
       Serial.println("[State] L1_RECEIVE");
       // +++ add code here and to the predefined function boolean l1_receive(int timeout) below
-      if (l1_receive(5000) == true){
-        Serial.println("frame recieved");
-      }
-      else {
-        Serial.println("timeout reached");
-      }
-      state = L2_FRAME_REC;
+	  
       // ---
       break;
 
     case L2_DATA_SEND:
       Serial.println("[State] L2_DATA_SEND");
       // +++ add code here
-      tx.frame_to = 0;
-      tx.frame_from = 0;
-      tx.frame_type = FRAME_TYPE_DATA;
-      tx.frame_seqnum = 0;
-      tx.frame_crc = 0;
-      //tx.frame_payload = 12;
-      tx.frame_payload = LED_PAYLOAD;
-      tx.frame_generation();
-      
-      state = L1_SEND;
+ 
       // ---
       break;
 
@@ -122,10 +97,7 @@ void loop() {
     case L2_FRAME_REC:
       Serial.println("[State] L2_FRAME_REC");
       // +++ add code here
-      //Serial.println(read_)
-      rx.frame = recFrame;
-      rx.frame_decompose();
-      state = HALT;
+
       // ---
       break;
 
@@ -139,24 +111,21 @@ void loop() {
     case L2_ACK_REC:
       Serial.println("[State] L2_ACK_REC");
       // +++ add code here
-      
+
       // ---
       break;
 
     case APP_PRODUCE:
       Serial.println("[State] APP_PRODUCE");
       // +++ add code here
-      LED_PAYLOAD = sh.select_led();
-      state = L2_DATA_SEND;
-
+    
       // ---
       break;
 
     case APP_ACT:
       Serial.println("[State] APP_ACT");
       // +++ add code here
-      Serial.println(sh.select_led());
-    
+
       // ---
       break;
 
@@ -179,35 +148,11 @@ void loop() {
 //
 
 void l1_send(unsigned long frame, int framelen) {
-  send_byte(PREAMBLE_SEQ);
-  send_byte(SFD_SEQ);
 
-  for (int i = framelen-1; i >= 0; i--) {
-    int bitVal = (frame >> i) & 1;
-    digitalWrite(PIN_TX, bitVal ? HIGH : LOW);
-    delay(T_S);  // 100ms per bit
-  }
 }
 
 boolean l1_receive(int timeout) {
-  recFrame = 0;
-  long time = millis();
-  int foundBit;
-  bool preambleOK = detect_byte(PREAMBLE_SEQ, timeout);
-  if (preambleOK){
-    Serial.println("Preamble found");
-    bool SFDOK = detect_byte(SFD_SEQ, timeout);
-    if (SFDOK){
-      Serial.println("SFD found");
-      // decoding frame
-      for (int i = 0; i <= 31; i++) {
-        foundBit = sh.sampleRecCh(PIN_RX);
-        recFrame = (recFrame << 1) | foundBit;
-        delay(T_S);  // 100ms per bit
-      }
-      Serial.println(recFrame);
-    }
-  }
+	
 	return true;
 }
 
@@ -215,56 +160,3 @@ boolean l1_receive(int timeout) {
 //
 // Add your functions here
 //
-
-void send_byte(byte data) {
-  for (int i = 7; i >= 0; i--) {
-    // Extract bit i (most significant bit first)
-    int bitVal = (data >> i) & 1;
-    digitalWrite(PIN_TX, bitVal ? HIGH : LOW);
-    delay(T_S);  // 100ms per bit
-  }
-}
-
-long read_next_bits(long data, int length){
-  long result = 0;
-  for (int i = length-1; i >= 0; i--) {
-    int bits = (data >> i) & 1;
-    result = result + bits*pow(2,i);
-  }
-  return result;
-}
-
-//void record_frame(long data, int framelen){
-//  for (int i = framelen-1; i >= 0; i--) {
-//      int bitVal = (frame >> i) & 1;
-//}
-
-bool detect_byte(byte wantedByte, int timeout){
-  long time = millis();
-  bool synched = false;
-  Serial.print("Searching for byte: ");
-  Serial.println(wantedByte);
-  while (synched == false){
-    byte foundByte = 0;
-    for (int i = 7; i >= 0; i--) {
-      int correctBit = (wantedByte >> i) & 1;
-      int foundBit = sh.sampleRecCh(PIN_RX);
-      if (correctBit != foundBit){
-        break;
-      }
-      foundByte = (foundByte << 1) | foundBit;
-      if (i == 0){
-        synched = true;
-        Serial.print("Found byte: ");
-        Serial.println(foundByte);
-      }
-      delay(T_S);  // 100ms per bit
-    }
-    if (millis()-time >= timeout){
-      Serial.println("timeout reached");
-      return false;
-    }
-  }
-	return true;
-}
-
